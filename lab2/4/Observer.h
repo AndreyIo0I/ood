e4e8ee1e75f8f2bae92cf7c5563e0937645dc966
Andrey Arsbkv <andrbeckov@gmail.com>
2021-09-27 09:24:07 +0300

    2_4

---
+++ lab2/4/Observer.h
@@ -0,0 +1,104 @@
+﻿#pragma once
+
+#include <set>
+#include <functional>
+#include <map>
+
+template <typename T>
+class IObservable;
+
+/*
+Шаблонный интерфейс IObserver. Его должен реализовывать класс, 
+желающий получать уведомления от соответствующего IObservable
+Параметром шаблона является тип аргумента,
+передаваемого Наблюдателю в метод Update
+*/
+template <typename T>
+class IObserver
+{
+public:
+	virtual void Update(IObservable<T>*, T const& data) = 0;
+	virtual ~IObserver() = default;
+};
+
+/*
+Шаблонный интерфейс IObservable. Позволяет подписаться и отписаться на оповещения, а также
+инициировать рассылку уведомлений зарегистрированным наблюдателям.
+*/
+template <typename T>
+class IObservable
+{
+public:
+	virtual ~IObservable() = default;
+	virtual void RegisterObserver(IObserver<T> & observer, int priority = 0) = 0;
+	virtual void NotifyObservers() = 0;
+	virtual void RemoveObserver(IObserver<T> & observer) = 0;
+};
+
+// Реализация интерфейса IObservable
+template <class T>
+class CObservable : public IObservable<T>
+{
+public:
+	typedef IObserver<T> ObserverType;
+
+	CObservable()
+	:m_observers([this](ObserverType* l, ObserverType* r)
+		{
+			return m_observersPriority[l] != m_observersPriority[r]
+				? m_observersPriority[l] < m_observersPriority[r]
+				: l < r;
+		})
+	{
+	}
+
+	void RegisterObserver(ObserverType & observer, int priority = 0) override
+	{
+		m_observersPriority[&observer] = priority;
+		m_observers.insert(&observer);
+	}
+
+	void NotifyObservers() override
+	{
+		m_notifyingObservers = true;
+		T data = GetChangedData();
+		for (auto & observer : m_observers)
+		{
+			observer->Update(this, data);
+		}
+		EraseObservers();
+		m_notifyingObservers = false;
+	}
+
+	void RemoveObserver(ObserverType & observer) override
+	{
+		if (m_notifyingObservers)
+		{
+			m_observersToErase.insert(&observer);
+		}
+		else {
+			m_observers.erase(&observer);
+		}
+	}
+
+protected:
+	// Классы-наследники должны перегрузить данный метод, 
+	// в котором возвращать информацию об изменениях в объекте
+	virtual T GetChangedData()const = 0;
+
+private:
+	void EraseObservers()
+	{
+		for (auto & observer : m_observersToErase)
+		{
+			m_observers.erase(observer);
+		}
+		m_observersToErase.clear();
+	}
+
+	std::map<ObserverType*, int> m_observersPriority;
+	std::set<ObserverType*, std::function<bool(ObserverType*, ObserverType*)>> m_observers;
+
+	std::set<ObserverType*> m_observersToErase;
+	bool m_notifyingObservers = false;
+};
---
+++ lab2/4/test.cpp
@@ -0,0 +1,102 @@
+#define CATCH_CONFIG_MAIN
+#include "../catch.hpp"
+#include "Observer.h"
+#include <vector>
+
+using namespace std;
+
+vector<int> order;
+vector<int> observableOrder;
+
+class CTestObservable : public CObservable<bool>
+{
+public:
+	void SetMeasurements(bool value)
+	{
+		m_value = value;
+		NotifyObservers();
+	}
+protected:
+	bool GetChangedData()const override
+	{
+		return m_value;
+	}
+private:
+	bool m_value = false;
+};
+
+class CObserver: public IObserver<bool>
+{
+public:
+	explicit CObserver(CTestObservable & observable, int id = 0)
+	: m_observable1(&observable)
+	, m_observable2(&observable)
+	, m_id(id)
+	{
+	}
+
+	explicit CObserver(CTestObservable & observable1, CTestObservable & observable2, int id = 0)
+		: m_observable1(&observable1)
+		, m_observable2(&observable2)
+		, m_id(id)
+	{
+	}
+
+	void Update(IObservable<bool>* observable, bool const& data) override
+	{
+		order.push_back(m_id);
+		if (observable == m_observable1)
+		{
+			observableOrder.push_back(1);
+			m_observable1->RemoveObserver(*this);
+		}
+		else if (observable == m_observable2)
+		{
+			observableOrder.push_back(2);
+			m_observable1->RemoveObserver(*this);
+		}
+	}
+
+private:
+	CTestObservable *m_observable1, *m_observable2;
+	int m_id = 0;
+};
+
+TEST_CASE("Observer removes themself from update fn")
+{
+	CTestObservable observable;
+	CObserver observer1(observable);
+	CObserver observer2(observable);
+
+	observable.RegisterObserver(observer1);
+	observable.RegisterObserver(observer2);
+
+	CHECK_NOTHROW(observable.SetMeasurements(true));
+}
+
+TEST_CASE("Use observers with priority")
+{
+	CTestObservable observable;
+	CObserver observer1(observable, 1);
+	CObserver observer2(observable, 2);
+
+	observable.RegisterObserver(observer1);
+	observable.RegisterObserver(observer2);
+
+	observable.SetMeasurements(true);
+	CHECK((order[0] == 2 && order[1] == 1));
+}
+
+TEST_CASE("Use observers with different observables")
+{
+	CTestObservable observable1, observable2;
+	CObserver observer(observable1, observable2, 1);
+
+	observable1.RegisterObserver(observer);
+	observable2.RegisterObserver(observer);
+
+	observable2.SetMeasurements(true);
+	observable1.SetMeasurements(false);
+
+	CHECK((observableOrder[0] == 2 && observableOrder[1] == 1));
+}

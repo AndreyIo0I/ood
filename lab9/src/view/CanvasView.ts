31b2405429ca1c1799743074d0241ce95495b9fe
Andrey Arsbkv <andrbeckov@gmail.com>
2022-04-30 15:53:00 +0300

    lab9 simplified CanvasView

--- lab9/src/view/CanvasView.ts
+++ lab9/src/view/CanvasView.ts
@@ -2,11 +2,6 @@ import {ShapeView} from './ShapeView'
 import {View} from './View'
 
 class CanvasView extends View<SVGSVGElement> {
-	//todo лишние поля
-	private width: number
-	private height: number
-	private shapes: Array<ShapeView> = []
-
 	constructor(width: number, height: number) {
 		super(document.createElementNS('http://www.w3.org/2000/svg', 'svg'))
 		this.element.classList.add('canvas')
@@ -15,22 +10,14 @@ class CanvasView extends View<SVGSVGElement> {
 	}
 
 	addShape(shape: ShapeView): void {
-		this.shapes.push(shape)
 		shape.render(this.getElement())
 	}
 
-	deleteShape(shape: ShapeView): void {
-		this.shapes = this.shapes.filter(v => v !== shape)
-		shape.remove()
-	}
-
 	setWidth(width: number): void {
-		this.width = width
 		this.element.style.width = `${width}px`
 	}
 
 	setHeight(height: number): void {
-		this.height = height
 		this.element.style.height = `${height}px`
 	}
 }
